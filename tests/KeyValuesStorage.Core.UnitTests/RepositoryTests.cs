af028f15cf92ea8ad10a604282008539c2797a70
Bronislav Svoboda <bassclide@gmail.com>
2026-05-16 21:21:09 +0200

    Adds unit tests projects

---
+++ tests/KeyValuesStorage.Core.UnitTests/RepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using KeyValueStorage.Core.Services;
+
+namespace KeyValuesStorage.Core.UnitTests;
+
+public class Tests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void ProvideData()
+	{
+		var repository = new Repository();
+		Assert.Pass();
+	}
+}
